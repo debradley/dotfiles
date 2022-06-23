2b68c184b35967c96f632893d30d18f152576e5b
Dan Bradley <dbradley2@wayfair.com>
2022-06-23 13:47:32 -0400

    Update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,12 +2,9 @@
 ;; Packages
 ;;;;
 
-;; Define package repositories
-(require 'package)
+;; Bugfix
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
